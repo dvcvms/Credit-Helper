29dde2fc6de6373ca76f9064b4cdf04802e942e4
dvcvms <tetraedrtrapecia@gmail.com>
2023-04-20 14:24:25 +0300

    Initial commit

---
+++ src/main/java/ru/mts/credit_registration/CreditRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.credit_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mts/credit_registration/CreditRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.credit_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
