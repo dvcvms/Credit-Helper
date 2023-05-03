2b752a80366f29c64bf48f106662a0acbc9ca78b
dvcvms <tetraedrtrapecia@gmail.com>
2023-05-04 00:14:14 +0300

    feat: add auth response, request; auth service

---
+++ src/main/java/ru/mts/credit_registration/model/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package ru.mts.credit_registration.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    private String token;
+    private String username;
+}
