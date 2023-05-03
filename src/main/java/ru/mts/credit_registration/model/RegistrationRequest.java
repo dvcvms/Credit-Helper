7a2d14250062c4bea7ef7e1737ddefad1c582199
dvcvms <tetraedrtrapecia@gmail.com>
2023-05-04 00:22:56 +0300

    feat: add registration service

---
+++ src/main/java/ru/mts/credit_registration/model/RegistrationRequest.java
@@ -0,0 +1,18 @@
+package ru.mts.credit_registration.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegistrationRequest {
+    private String username;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+}
