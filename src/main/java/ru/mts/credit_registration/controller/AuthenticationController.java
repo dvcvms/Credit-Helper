f1d3b1493efa3f23d75b07ce017c48a4fd28ff8c
dvcvms <tetraedrtrapecia@gmail.com>
2023-05-04 00:31:36 +0300

    feat: add security config; auth, registration controllers

---
+++ src/main/java/ru/mts/credit_registration/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package ru.mts.credit_registration.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mts.credit_registration.model.AuthenticationRequest;
+import ru.mts.credit_registration.model.AuthenticationResponse;
+import ru.mts.credit_registration.serivce.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
