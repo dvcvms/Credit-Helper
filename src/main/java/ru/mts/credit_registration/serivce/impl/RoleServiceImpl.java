6b8a4ffe2886615be225edf71aee0dd534111d8d
dvcvms <tetraedrtrapecia@gmail.com>
2023-05-03 23:56:56 +0300

    feat: add role service

---
+++ src/main/java/ru/mts/credit_registration/serivce/impl/RoleServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.mts.credit_registration.serivce.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.mts.credit_registration.entity.RoleEntity;
+import ru.mts.credit_registration.exception.RoleNotFoundException;
+import ru.mts.credit_registration.repository.impl.RoleRepositoryImpl;
+import ru.mts.credit_registration.serivce.RoleService;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepositoryImpl roleRepository;
+
+    @Override
+    public RoleEntity findByName(String name) {
+        return roleRepository.findByName(name).orElseThrow(() -> new RoleNotFoundException("ROLE_NOT_FOUND",
+                        String.format("Роль `%s` не найдена", name)
+                )
+        );
+    }
+}
